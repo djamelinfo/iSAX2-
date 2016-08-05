0ff82bd9385869b939b5a8be10d63a8ffebca4ba
Djamel Edine <yagoubi.djamel@gmail.com>
2016-08-05 13:11:20 +0200

    Add files via upload

---
+++ org/timeseries/Exemple2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.timeseries.exemple;
+
+import java.io.File;
+import java.io.IOException;
+import org.timeseries.isax2.iSAXIndex;
+
+/**
+ *
+ * @author djamel
+ */
+public class Exemple {
+
+    public static void main(String[] args) throws NumberFormatException,
+                                                  IOException, Exception {
+
+        if(args.length < 4) {
+            System.err.println(
+                    "Usage: iSAXindex <numberOfTs> <sizeOfTs> <indexHome> <DataSet1> <DataSet2>");
+            System.exit(1);
+        }
+
+        iSAXIndex index = new iSAXIndex(Integer.parseInt(args[1]), (short) 8,
+                                        (short) 512, (short) 2, (short) 100,
+                                        args[2]);
+
+        File f = new File(args[2]);
+        if(f.isDirectory()) {
+            File[] list = f.listFiles();
+            for(File list1 : list) {
+                list1.delete();
+            }
+        }
+        boolean b = true;
+
+        double start = System.currentTimeMillis();
+
+        //System.out.println(index.ConstructionOfIndex("/media/sda5/time series/Seismic_data.dataNormalize.csv", Integer.parseInt(args[0]), 256));
+        System.out.println(index.ConstructionOfIndex(args[3], Integer.parseInt(
+                                                     args[0]), Integer.parseInt(
+                                                             args[1]), false));
+        if(args.length == 5) {
+            System.out.println(index.ConstructionOfIndex(args[4], Integer.
+                                                         parseInt(args[0]),
+                                                         Integer.parseInt(
+                                                                 args[1]), true));
+        }
+
+        double end = System.currentTimeMillis();
+        System.out.println(index);
+        System.out.println("time to builde index = " + (end - start) + "ms");
+        //System.out.println("++++++++++++++++++++++++++++++++++++++++++++index+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        index.saveIndexInDisk();
+    }
+
+}
